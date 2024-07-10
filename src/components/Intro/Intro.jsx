6ad8cd5e4b85d157c77f6fba98c2fb0f8a89b42c
GraceNanguy <nanguy_maria@imatabidjan.com>
2024-07-10 15:26:24 +0000

    Description des modifications

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -35,7 +35,7 @@ const Intro = () => {
           </span>
         </div>
         <Link to="contact" smooth={true} spy={true}>
-          <button className="button i-button">Hire me</button>
+          <button className="button i-button">Contactez-moi</button>
         </Link>
         {/* social icons */}
         <div className="i-icons">
